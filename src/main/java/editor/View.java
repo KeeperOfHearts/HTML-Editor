d93c1e92f85ecb3c73e9993d061a12c0ec590c40
Wsieslaw Mackiewicz <wsieslaw.mackiewicz@gmail.com>
2025-01-25 21:36:12 +0100

    Implmented initEditor method in View

--- src/main/java/editor/View.java
+++ src/main/java/editor/View.java
@@ -1,7 +1,9 @@
 package editor;
 
 import editor.listeners.FrameListener;
+import editor.listeners.TabbedPaneChangeListener;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
@@ -40,7 +42,15 @@ public class View extends JFrame implements ActionListener {
 
     }
     public void initEditor() {
-
+        htmlTextPane.setContentType("text/html");
+        JScrollPane htmlScrollPane = new JScrollPane(htmlTextPane);
+        tabbedPane.add("HTML", htmlScrollPane);
+        JScrollPane textScrollPane = new JScrollPane(plainTextPane);
+        tabbedPane.add("Текст", textScrollPane);
+        tabbedPane.setPreferredSize(new Dimension(300, 300));
+        TabbedPaneChangeListener tabbedPaneChangeListener = new TabbedPaneChangeListener(this);
+        tabbedPane.addChangeListener(tabbedPaneChangeListener);
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
     }
     public void initGui() {
         initMenuBar();
