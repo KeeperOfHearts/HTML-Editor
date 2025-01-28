d464c00a0e9f5b283bda1a0a4b50922dd40a6853
Wsieslaw Mackiewicz <wsieslaw.mackiewicz@gmail.com>
2025-01-28 20:10:21 +0100

    Implemented all action classes

--- src/main/java/editor/action/RedoAction.java
+++ src/main/java/editor/action/RedoAction.java
@@ -6,12 +6,13 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 public class RedoAction extends AbstractAction {
+    private View view;
 
     public RedoAction(View view) {
-
+        this.view = view;
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        view.redo();
     }
 }
